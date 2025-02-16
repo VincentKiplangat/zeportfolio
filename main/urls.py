d30144995ed71748903120b35d0e2bfefcf13967
Vincent Kiplangat <88483461+VincentKiplangat@users.noreply.github.com>
2025-02-16 22:10:15 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,13 +4,13 @@ from .views import contact_form
 
 urlpatterns = [
     path('', views.home, name="index-main"),
-    path('blog', views.blog, name="single-blog"),
-    path('blog2', views.blog2, name="single-blog2"),
-    path('blog3', views.blog3, name="single-blog3"),
-    path('blog4', views.blog4, name="single-blog4"),
-    path('blog5', views.blog5, name="single-blog5"),
-    path('blog6', views.blog6, name="single-blog6"),
+    path('blog/Technology', views.blog, name="single-blog"),
+    path('blog/Cybersecurity', views.blog2, name="single-blog2"),
+    path('blog/Artificial Inteligence', views.blog3, name="single-blog3"),
+    path('blog/Sustainability', views.blog4, name="single-blog4"),
+    path('blog/Cloud Computing', views.blog5, name="single-blog5"),
+    path('blog/Workplace Trend', views.blog6, name="single-blog6"),
     path('error_404', views.error_404, name="error-404"),
-    path("submit/", contact_form, name="contact_form"),
+    path("contact/submit/", views.contact_form, name="contact_submit"),
 
 ]
